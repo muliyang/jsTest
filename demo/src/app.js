7fbf4f2fac7887dbbea6aa4833944042bab26999
muliyang <muliyang@pinming.cn>
2021-06-22 08:38:04 +0800

    init react-hooks

---
+++ demo/src/app.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import Count from './count';
+import MemoTest from './useMemo';
+
+const App = ()=>{
+    return <>
+        <Count />
+        <MemoTest />
+    </>
+}
+
+export default App;
+
